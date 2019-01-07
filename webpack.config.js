26d9d94f59dbddf55ade3d93f92d2d589b5c96cc
Amal Jossy <amaljossy1@gmail.com>
2019-01-08 00:22:17 +0530

    React production optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
-
-module.exports = {
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var baseConfig = {
   entry: './app/src/app.js',
   mode: 'development',
   output: {
@@ -15,5 +16,17 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [],
+  optimization: {
+    minimizer:[]
+  },
 };
+if (process.env.NODE_ENV === 'production') {
+  baseConfig.plugins.push(new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }));
+  baseConfig.optimization.minimizer.push(new UglifyJsPlugin());
+  console.log("Using production plugins");
+}
+module.exports = baseConfig;
